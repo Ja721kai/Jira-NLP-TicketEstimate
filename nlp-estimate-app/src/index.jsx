68acffb1e115548d297b58618b70808082ad7396
Jan Kaiser <jan.kaiser@devs-group.ch>
2021-03-10 23:20:25 +0100

    Add custom text

--- nlp-estimate-app/src/index.jsx
+++ nlp-estimate-app/src/index.jsx
@@ -3,7 +3,8 @@ import ForgeUI, { render, Fragment, Text, IssuePanel } from '@forge/ui';
 const App = () => {
   return (
     <Fragment>
-      <Text>Hello world!</Text>
+      <Text>Hello Mahtab & Sabrina, this is your local developer from Constance who just setup his developing
+      environment to create and continuously deploy forge apps through Docker. </Text>
     </Fragment>
   );
 };
