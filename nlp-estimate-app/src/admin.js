85855f0f3c5f1356edf4fe2f07a8ead884e21ca0
Jan Kaiser <jan_kaiser69@outlook.de>
2021-10-14 06:50:54 +0200

    Import component into Admin Panel

--- nlp-estimate-app/src/admin.js
+++ nlp-estimate-app/src/admin.js
@@ -1,9 +1,11 @@
 import ForgeUI, { AdminPage, render, Text } from "@forge/ui";
+import {BerichteComponent} from "./components/berichteErstellen";
 
 export const AdminConfigPage = () => {
     return (
         <AdminPage>
           <Text> Hello World! </Text>
+          <BerichteComponent />
         </AdminPage>
     );
 };
---
+++ nlp-estimate-app/src/components/berichteErstellen.js
@@ -0,0 +1,9 @@
+import ForgeUI, {render, Fragment, Text} from '@forge/ui';
+
+export const BerichteComponent = () => {
+  return (
+    <Fragment>
+      <Text> Hi, ich generiere bald Berichte. </Text>
+    </Fragment>
+  );
+};
