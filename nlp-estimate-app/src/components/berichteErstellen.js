39622d4e6c5d85537e283764b06bbfa37131f1b7
Jan Kaiser <jan_kaiser69@outlook.de>
2021-10-16 10:14:39 +0200

    Add table with mock data

--- nlp-estimate-app/src/components/berichteErstellen.js
+++ nlp-estimate-app/src/components/berichteErstellen.js
@@ -1,9 +1,67 @@
-import ForgeUI, {render, Fragment, Text} from '@forge/ui';
+import ForgeUI, {Table, Head, Row, Cell, Text, Button, Fragment} from '@forge/ui';
+
+const tableColumnNames = ["Name", "Besitzer", "Zugriff", "Markiert von"];
+
+const mockReports = [
+  {
+    name: "Bericht 1",
+    owner: "Mahtab Houshmand",
+    access: "public",
+    marked_by: 0
+  },
+  {
+    name: "Bericht 2",
+    owner: "Sabrina Schuler",
+    access: "private",
+    marked_by: 2
+  }
+];
+
 
 export const BerichteComponent = () => {
   return (
     <Fragment>
-      <Text> Hi, ich generiere bald Berichte. </Text>
+      <Text>{getPaginationString(mockReports, 20) + " von " + mockReports.length.toString()}</Text>
+      <Table>
+        <Head>
+          <Cell>
+            <Button
+              icon="star-filled"
+              disabled={true}
+            />
+          </Cell>
+          {tableColumnNames.map(columnName => (
+            <Cell>
+              <Text>{columnName}</Text>
+            </Cell>
+          ))}
+        </Head>
+        {mockReports.map(report => (
+          <Row>
+            <Cell>
+              <Button
+                icon={"star"}
+                onClick={async () => {
+                  console.log("Hello World!")
+                }}
+              />
+            </Cell>
+            <Cell>
+              <Text>{report.name}</Text>
+            </Cell>
+            <Cell>
+              <Text>{report.owner}</Text>
+            </Cell>
+            <Cell>
+              <Text>{report.access}</Text>
+            </Cell>
+            <Cell>
+              <Text>{report.marked_by.toString() + " Personen"}</Text>
+            </Cell>
+          </Row>
+        ))}
+      </Table>
     </Fragment>
+
   );
 };
